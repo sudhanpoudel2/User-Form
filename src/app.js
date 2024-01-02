8d049bd2a307946ca12853df2ba4c22ad4473dd1
Pujan Bohora <bohorapawan@gmail.com>
2024-01-02 19:41:49 +0545

    get user data

--- src/app.js
+++ src/app.js
@@ -11,40 +11,42 @@ app.get('/getUserData',async (req,res)=>{
     //req = send file from front to back
     //res = send file from back to front
     console.log("hello");
-    // try {
-    //   // Find all users
-    //   const users = await UserModel.find();
+    try {
+      // Find all users
+      const users = await UserModel.find();
+
+      console.log(users);
   
-    //   res.json(users);
-    // } catch (error) {
-    //   console.error(error);
-    //   res.status(500).json({ message: 'Internal server error' });
-    // }
+      res.json(users);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
     
 });
 
-app.put('/data',(req,res)=>{
+app.put('/updateUser',(req,res)=>{
   const formData = req.body;
   console.log(formData);
 })
 
-// app.post('/data', async (req, res) => {
-//     try {
-//       const formData = req.body;
-//       console.log(formData);
+app.post('/data', async (req, res) => {
+    try {
+      const formData = req.body;
+      console.log(formData);
   
-//       // Create a new user document using the UserModel
-//       const newUser = new UserModel(formData);
+      // Create a new user document using the UserModel
+      const newUser = new UserModel(formData);
   
-//       // Save the document to the database
-//       await newUser.save();
+      // Save the document to the database
+      await newUser.save();
   
-//       res.status(200).json({ message: 'Form submitted successfully' });
-//     } catch (error) {
-//       console.error('Error saving to MongoDB:', error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
-//   });
+      res.status(200).json({ message: 'Form submitted successfully' });
+    } catch (error) {
+      console.error('Error saving to MongoDB:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
   
 
 app.listen(process.env.PORT || 5000 , ()=>{
