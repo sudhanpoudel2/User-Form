82b39ccff668010c1977987f7a73e119cf39a531
sudhanpoudel2@gmail.com <sudhanpoudel2@gmail.com>
2024-01-02 19:32:25 +0545

    done

--- src/app.js
+++ src/app.js
@@ -7,33 +7,44 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    console.log("hello world");
-    res.send("hi this is sudhna");
+app.get('/getUserData',async (req,res)=>{
+    //req = send file from front to back
+    //res = send file from back to front
+    console.log("hello");
+    // try {
+    //   // Find all users
+    //   const users = await UserModel.find();
+  
+    //   res.json(users);
+    // } catch (error) {
+    //   console.error(error);
+    //   res.status(500).json({ message: 'Internal server error' });
+    // }
+    
 });
 
-// app.put('/',(req,res)=>{
-//   const formData = req.body;
-//   console.log(formData);
-// })
+app.put('/data',(req,res)=>{
+  const formData = req.body;
+  console.log(formData);
+})
 
-app.post('/data', async (req, res) => {
-    try {
-      const formData = req.body;
-      console.log(formData);
+// app.post('/data', async (req, res) => {
+//     try {
+//       const formData = req.body;
+//       console.log(formData);
   
-      // Create a new user document using the UserModel
-      const newUser = new UserModel(formData);
+//       // Create a new user document using the UserModel
+//       const newUser = new UserModel(formData);
   
-      // Save the document to the database
-      await newUser.save();
+//       // Save the document to the database
+//       await newUser.save();
   
-      res.status(200).json({ message: 'Form submitted successfully' });
-    } catch (error) {
-      console.error('Error saving to MongoDB:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+//       res.status(200).json({ message: 'Form submitted successfully' });
+//     } catch (error) {
+//       console.error('Error saving to MongoDB:', error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   });
   
 
 app.listen(process.env.PORT || 5000 , ()=>{
